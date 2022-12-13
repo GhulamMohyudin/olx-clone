8d0badcd10790e7f0b9da48b6b674cd281645175
GhulamMohyuldin <gmdservices777@gmail.com>
2022-12-12 23:00:23 -0800

    com

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import CategoryAndPage from "./components/CategoryAndPags";
 import Carousal from "./components/Carousal";
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Routes>
+      <Routes>
         <Route
           path="/"
           exact
@@ -35,9 +35,9 @@ function App() {
             }
           />
         </Route>
-      </Routes> */}
+      </Routes>
 
-      <Header
+      {/* <Header
         img="logoOne.png"
         className="logo1"
         list1="Moters"
@@ -48,7 +48,7 @@ function App() {
         className="logo2"
         placeholder1="Pakistan"
         placeholder2="Find Cars, Mobile Phones and more..."
-      />
+      /> */}
       <CategoryAndPage />
       <Carousal img="https://images.olx.com.pk/thumbnails/310378076-800x600.webp" />
     </>
